9a317e4713fb9994f044fe69070dc6ea642698a1
metaute73 <79713985+metaute73@users.noreply.github.com>
2025-02-18 17:17:38 -0500

    first commit

---
+++ src/test/java/com/egg/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.egg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
