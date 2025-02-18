000ebac5bda5c58360949a25e493064c24fce875
metaute73 <79713985+metaute73@users.noreply.github.com>
2025-02-18 18:14:22 -0500

    todo actualizado

--- src/main/java/com/egg/persistencia/EditorialDAO.java
+++ src/main/java/com/egg/persistencia/EditorialDAO.java
@@ -1,5 +1,29 @@
 package com.egg.persistencia;
+import com.egg.entidades.Editorial;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class EditorialDAO {
-    
+
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ViveroPU");
+    private final EntityManager em = emf.createEntityManager();
+
+    public void guardarEditorial(Editorial editorial) throws Exception {
+        em.getTransaction().begin();
+        em.persist(editorial);
+        em.getTransaction().commit();
+    }
+    public void modificarEditorial()
+    public void eliminarEditorial(Integer id) throws Exception {
+        Editorial editorial = buscarEditorial(id);
+        em.getTransaction().begin();
+        em.remove(editorial);
+        em.getTransaction().commit();
+    }
+
+    public Editorial buscarEditorial(int id) {
+        return em.find(Editorial.class, id);
+    }
+
 }
