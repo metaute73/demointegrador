b5ada9adf8bcdb917451b0c072d69ffba9f004fe
vicuartas230 <vicuartas230@gmail.com>
2025-02-19 17:08:08 -0500

    Subir libro servicio

--- src/main/java/com/egg/servicios/LibroServicio.java
+++ src/main/java/com/egg/servicios/LibroServicio.java
@@ -1,5 +1,33 @@
 package com.egg.servicios;
 
+import com.egg.entidades.Libro;
+import com.egg.persistencia.LibroDAO;
+
 public class LibroServicio {
+    private LibroDAO libroDAO;
     
+    public LibroServicio() {
+        this.libroDAO = new LibroDAO();
+    }
+
+    public void guardarLibro(Libro libro) {
+        try {
+            libroDAO.guardarLibro(libro);
+            System.out.println("Libro guardado con éxito.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void buscarLibro(Long isbn) {
+        Libro libroRequerido = libroDAO.buscarLibro(isbn);
+        try {
+            if (libroRequerido == null) {
+                throw new Exception("No se encontró el libro con ISBN " + isbn);
+            }
+            System.out.println(libroRequerido);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
